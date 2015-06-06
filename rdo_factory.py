55a9db29cc6f44c03cf1343af1c383f33283a134
Adam Young <ayoung@redhat.com>
2015-06-05 21:30:38 -0400

    Initial create

---
+++ rdo_factory.py
@@ -0,0 +1,120 @@
+import logging
+import os
+
+from keystoneclient.v3 import client as keystoneclient
+from neutronclient.neutron import client as neutronclient
+from novaclient import client as novaclient
+
+
+from keystoneclient import session as ksc_session
+from keystoneclient.auth.identity import v3
+from keystoneclient.v3 import client as keystone_v3
+
+
+network_name='rdo-net'
+subnet_name='rdo-subnet'
+
+
+class WorkItem(object):
+    def __init__(self, neutron):
+        self.neutron = neutron
+
+class Network(WorkItem):                
+    def cleanup(self):
+        for network in self.neutron.list_networks(name=network_name)['networks']:
+            self.neutron.delete_network(network['id'])
+        
+
+    def create(self):    
+        #openstack network create ayoung-private
+        network = {'name': network_name, 'admin_state_up': True}
+        self.neutron.create_network({'network':network})
+            
+
+class SubNet(WorkItem):                
+    def cleanup(self):
+        for subnet in self.neutron.list_subnets(name=subnet_name)['subnets']:
+            self.neutron.delete_subnet(subnet['id'])
+
+    def create(self):    
+        #neutron  subnet-create ayoung-private 192.168.52.0/24 --name ayoung-subnet1
+        for network in self.neutron.list_networks(name=network_name)['networks']:
+            print (network['name'])
+
+        body_create_subnet = {
+            "subnets": [
+                {
+                    "name": subnet_name,
+                    "cidr": "192.168.52.0/24",
+                    "ip_version": 4,
+                    "network_id": network['id']
+                }
+            ]
+        }
+        
+        subnet = self.neutron.create_subnet(body=body_create_subnet)
+
+router_name = 'rdo-router'
+        
+class Router(WorkItem):                    
+    def cleanup(self):
+        for router in self.neutron.list_routers(name=router_name)['routers']:
+            self.neutron.delete_router(router['id'])
+
+    def create(self):    
+            body_value = {'router': {
+                'name' : router_name,
+                'admin_state_up': True,
+            }}
+            router = self.neutron.create_router(body=body_value)
+        
+        
+#neutron router-create ayoung-private-router
+#external_id=`neutron net-show external | awk '/ id / {print $4}'`
+#neutron router-interface-add $external_id
+        
+
+        
+        
+#logging.basicConfig(level=logging.DEBUG)
+
+OS_AUTH_URL = os.environ.get('OS_AUTH_URL') 
+OS_USERNAME = os.environ.get('OS_USERNAME') 
+OS_PASSWORD= os.environ.get('OS_PASSWORD')
+OS_USER_DOMAIN_NAME=os.environ.get('OS_USER_DOMAIN_NAME')
+OS_PROJECT_DOMAIN_NAME=os.environ.get('OS_PROJECT_DOMAIN_NAME')
+OS_PROJECT_NAME=os.environ.get('OS_PROJECT_NAME')
+
+auth = v3.Password(auth_url=OS_AUTH_URL,
+                   username=OS_USERNAME,
+                   user_domain_name=OS_USER_DOMAIN_NAME,
+                   password=OS_PASSWORD,
+                   project_name=OS_PROJECT_NAME,
+                   project_domain_name=OS_PROJECT_DOMAIN_NAME)
+
+
+session = ksc_session.Session(auth=auth)
+keystone = keystone_v3.Client(session=session)
+nova = novaclient.Client('2', session=session)
+neutron = neutronclient.Client('2.0', session=session)
+neutron.format = 'json'
+
+#for server in nova.servers.list():
+#    print (server)
+
+
+
+work_item_classes =[Network,SubNet,Router]
+work_items = []
+for item_class in work_item_classes:
+    work_items.append(item_class(neutron))
+
+
+for item in work_items:
+    item.create()
+
+import pdb; pdb.set_trace()
+    
+for item in reversed(work_items):
+    item.cleanup()
+
