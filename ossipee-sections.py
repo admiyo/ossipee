b3a0c0ffad41087328f460c837b2f8a68441fd36
Adam Young <ayoung@redhat.com>
2015-09-25 10:35:00 -0400

    enumerate sections

---
+++ ossipee-sections.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import os
+import re
+
+regex = re.compile('^\[.*')
+
+with open(os.environ['HOME'] + "/.ossipee/config.ini", "r") as config:
+    for line in config:
+        for section in regex.findall(line):
+            print re.sub('[\[\]]','' ,section)
